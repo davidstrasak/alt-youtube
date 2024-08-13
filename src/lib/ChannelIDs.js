22e433cba72f04f3914250f54c582739f87db848
davidstrasak <davidstrasak@seznam.cz>
2024-08-13 20:28:30 +0200

    start adding tags to channelIDs

--- src/lib/ChannelIDs.js
+++ src/lib/ChannelIDs.js
@@ -1,9 +1,15 @@
 export let ChannelIDs = [
 	{ name: "All", ID: "all" },
-	{ name: "Dungeon Craft", ID: "UCD6ERRdXrF2IZ0R888G8PQg" },
+	{ name: "Dungeon Craft", ID: "UCD6ERRdXrF2IZ0R888G8PQg", tag: ["TTRPGs"] },
 	{ name: "Real Engineering", ID: "UCR1IuLEqb6UEA_zQ81kwXfg" },
 	{ name: "Good Work", ID: "UC_-hYjoNe4PJNFa9iZ4lraA" },
 	{ name: "The Dungeon Coach", ID: "UCz8xkTADYdRQR7GP-f2PjOA" },
 	{ name: "Runehammer", ID: "UCCh5vto8JFstb9Sma9zV25g" },
-	{ name: "Bob World Builder", ID: "UCC8AgO4FbP11n_WBdFai7DA" }
+	{ name: "Bob World Builder", ID: "UCC8AgO4FbP11n_WBdFai7DA" },
+	{ name: "PyroLIVE", ID: "UCdc_JyNaB5VJ0gdWUtWfGDg" },
+	{ name: "Pyrocynical", ID: "UCN5xN8gIljvWYnsj06V8WbQ" },
+	{ name: "penguinz0", ID: "UCq6VFHwMzcMXbuKyG7SQYIg" },
+	{ name: "Dan Go", ID: "UCEzSFZRs8zJb5x5depOtUHA" },
+	{ name: "Fireship", ID: "UCsBjURrPoezykLs9EqgamOA" },
+	{ name: "Map Crow", ID: "UCoXApjHCg0x3EUcpNthI92g" }
 ];
