1f449db425fbb888c755bea131245d0782fde8f6
davidstrasak <davidstrasak@seznam.cz>
2024-08-18 17:19:47 +0200

    make the video larger

--- src/routes/videos/[...videoId]/+page.svelte
+++ src/routes/videos/[...videoId]/+page.svelte
@@ -3,12 +3,17 @@
 	export let data;
 </script>
 
-<!-- Embed the YouTube video using the captured videoId -->
-<iframe
-	title="video"
-	width="560"
-	height="315"
-	src={`https://www.youtube.com/embed/${data.videoId}?rel=0&autoplay=0`}
-	frameborder="0"
-	allowfullscreen
-></iframe>
+<div class="w-full">
+	<br />
+	<a href="/" class="btn btn-primary btn-outline btn-lg"> Go back </a>
+	<!-- Embed the YouTube video using the captured videoId -->
+	<iframe
+		class="mx-auto"
+		title="video"
+		width="1080"
+		height="608"
+		src={`https://www.youtube.com/embed/${data.videoId}?rel=0&autoplay=0`}
+		frameborder="0"
+		allowfullscreen
+	></iframe>
+</div>
